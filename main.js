65ff1ff04ba69720abb0f6e940418f0d581a0502
Brian Engelhardt <briatt22@gmail.com>
2017-07-18 20:33:22 -0500

    this should work!

---
+++ main.js
@@ -0,0 +1,50 @@
+var adjectives = [
+	'random',
+	'glitchy',
+	'yaoi',
+	'generic',
+	'pet looking',
+	'rainbow',
+	'ugly',
+	'beautiful',
+	'inverted',
+	'funky',
+	'foreign',
+	'testable'
+];
+var nouns = [
+	' poop',
+	' head',
+	' edits',
+	' alphabet'
+];
+var prepositions = [
+	' of doom',
+	' in space',
+	' in time',
+	''
+];
+function createOptions() {
+	var options = [];
+	for (var i = 0; i < 20; i++) {
+		options.push(
+			adjectives[Math.floor(Math.random()*adjectives.length)] +
+			nouns[Math.floor(Math.random()*nouns.length)] +
+			prepositions[Math.floor(Math.random()*prepositions.length)]
+		);
+	}
+	writeOptions(options);
+}
+function writeOptions(options) {
+	var ul = $('ul');
+	var lis = options.map(function (option) {
+		var li = $('li');
+		li.innerText = option;
+		return li;
+	});
+	for (var i = 0; i < lis.length; i++) {
+		ul.appendChild(lis[i]);
+	}
+	$('.target').innerHTML($(ul).innerHTML());
+}
+$('.generate').click(createOptions);
