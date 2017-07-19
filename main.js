7108bbd8ae3eb2bec28cade54635224edc244b4d
Brian Engelhardt <briatt22@gmail.com>
2017-07-18 21:01:26 -0500

    deduplicate and a bunch more options

--- main.js
+++ main.js
@@ -10,29 +10,88 @@ var adjectives = [
 	'inverted',
 	'funky',
 	'foreign',
-	'testable'
+	'testable',
+	'laser',
+	'gritty',
+	'old',
+	'ancient',
+	'creepy',
+	'holy',
+	'crazy',
+	'unholy',
+	'unfortunate',
+	'original',
+	'tasty',
+	'German',
+	'Egyptian',
+	'Atlantian',
+	'Greek',
+	'cheeky',
+	'Bohemian'
 ];
 var nouns = [
 	' poop',
 	' head',
 	' edits',
-	' alphabet'
+	' alphabet',
+	' eyes',
+	' laser',
+	' kitten',
+	' Jesus',
+	' devil',
+	' puppy',
+	' french fry',
+	' artist',
+	' hairspray',
+	' leader',
+	' giant',
+	' enchantress',
+	' cat tower',
+	' rainbow',
+	' Joker'
 ];
 var prepositions = [
 	' of doom',
 	' in space',
 	' in time',
+	' grasping at it\'s face',
+	' around itself',
+	' in goggles',
+	' at a party',
 	''
 ];
+function createOption() {
+	var adjective = Math.floor(Math.random()*adjectives.length),
+		noun = Math.floor(Math.random()*nouns.length),
+		preposition = Math.floor(Math.random()*prepositions.length),
+		total = adjective * 10000 + noun * 100 + preposition;
+	return {
+		adjective: adjective,
+		noun: noun,
+		preposition: preposition,
+		total: total
+	};
+}
 function createOptions() {
 	var options = [];
 	for (var i = 0; i < 20; i++) {
-		options.push(
-			adjectives[Math.floor(Math.random()*adjectives.length)] +
-			nouns[Math.floor(Math.random()*nouns.length)] +
-			prepositions[Math.floor(Math.random()*prepositions.length)]
-		);
+		var option = createOption();
+		var abort = 0;
+		for (var ii = 0; ii < options.length; ii++ ){
+			if (options[ii].total == option.total) {
+				option = createOption();
+				ii = 0;
+				abort = 1;
+			}
+			if (abort >= 100) {
+				break;
+			}
+		}
+		options.push(option);
 	}
+	options = options.map(function(option) {
+		return adjectives[option.adjective] + nouns[option.noun] + prepositions[options.preposition];
+	});
 	writeOptions(options);
 }
 function writeOptions(options) {
